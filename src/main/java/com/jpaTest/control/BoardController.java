80fe8e838f63bb3d4db83f7d326165a7b9468e1c
kyerim1 <nilzero25@gmail.com>
2024-08-20 12:51:55 +0900

    index css js

--- src/main/java/com/jpaTest/control/BoardController.java
+++ src/main/java/com/jpaTest/control/BoardController.java
@@ -1,9 +1,16 @@
 package com.jpaTest.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
+
+    // localhost:8080
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
 
 
 }
---
+++ src/main/resources/static/js/park.js
@@ -0,0 +1,15 @@
+
+$(function(){
+
+    $("#menuIcon").click(function(){
+          if( $("#header").hasClass("slide") ){
+                $("#header").removeClass("slide");
+                $("#main").removeClass("full");
+          }else{
+                 $("#header").addClass("slide");
+                 $("#main").addClass("full");
+          }
+    });
+
+
+});
